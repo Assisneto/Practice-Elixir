72e68f141eb4bcd27bab91d377f344562d6bfd4b
Assisneto <netodamasceno12@gmail.com>
2019-09-03 16:35:31 -0300

    baby step

--- app/test/app_test.exs
+++ app/test/app_test.exs
@@ -5,10 +5,15 @@ defmodule App.CalculatorTest do
   test "should return 4 when multiply 2 by 2" do
     assert App.Calculator.multiply(2,2) == 4
   end
+
+  test "should return 6 when multiply 2 by 3" do
+    assert App.Calculator.multiply(2,3) == 6
+  end
+
 end
 
 defmodule App.Calculator do
   def multiply(a, b) do
-    4  
+    a * b
   end
 end
