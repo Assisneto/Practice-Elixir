d6ac661f90c5d94b6595fde0f243d6f311049bb7
Assisneto <netodamasceno12@gmail.com>
2019-09-03 16:45:36 -0300

    refute

--- app/test/app_test.exs
+++ app/test/app_test.exs
@@ -10,10 +10,22 @@ defmodule App.CalculatorTest do
     assert App.Calculator.multiply(2,3) == 6
   end
 
+  test "should return nil when multiply 2 by nil" do
+    assert App.Calculator.multiply(2,nil) == nil
+  end
+  test "should return nil when multiply nil by 2" do
+    assert App.Calculator.multiply(nil,2) == nil
+  end
+  test "should return 10 when multiply 2 by 4" do
+    refute App.Calculator.multiply(2,4) == 10, "This is an error"
+  end
 end
 
 defmodule App.Calculator do
   def multiply(a, b) do
-    a * b
+    cond do
+      is_nil(a) || is_nil(b) -> nil
+      true -> a * b
+    end
   end
 end
